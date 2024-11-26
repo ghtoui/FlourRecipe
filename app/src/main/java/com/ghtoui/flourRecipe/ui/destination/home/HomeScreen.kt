760557dc1b4743844f7b028bfa6efa53d414f751
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-11-26 15:12:02 +0900

    lint修正

--- app/src/main/java/com/ghtoui/flourRecipe/ui/destination/home/HomeScreen.kt
+++ app/src/main/java/com/ghtoui/flourRecipe/ui/destination/home/HomeScreen.kt
@@ -5,25 +5,49 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.ghtoui.flourRecipe.core.ui.LocalMainNavController
 import com.ghtoui.flourRecipe.ui.destination.settings.navigateToSettings
+import com.ghtoui.flourRecipe.ui.theme.FlourRecipeTheme
 
 /**
  * ホーム画面
  */
 @Composable
-fun HomeScreen(
-    mainNavHostController: NavHostController = LocalMainNavController.current
+fun HomeScreen(mainNavHostController: NavHostController = LocalMainNavController.current) {
+    HomeScreen(
+        modifier = Modifier,
+        onClickButton = mainNavHostController::navigateToSettings,
+    )
+}
+
+@Composable
+private fun HomeScreen(
+    onClickButton: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Button(
         modifier = Modifier
             .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()),
-        onClick = mainNavHostController::navigateToSettings
+        onClick = onClickButton,
     ) {
         Text(text = "home")
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    FlourRecipeTheme {
+        Surface {
+            HomeScreen(
+                onClickButton = {},
+            )
+        }
     }
 }
--- app/src/main/java/com/ghtoui/flourRecipe/ui/destination/settings/SettingsScreen.kt
+++ app/src/main/java/com/ghtoui/flourRecipe/ui/destination/settings/SettingsScreen.kt
@@ -12,13 +12,12 @@ import androidx.navigation.NavHostController
 import com.ghtoui.flourRecipe.core.ui.LocalMainNavController
 
 @Composable
-fun SettingsScreen(
-    mainNavHostController: NavHostController = LocalMainNavController.current
-) {
+fun SettingsScreen(mainNavHostController: NavHostController = LocalMainNavController.current) {
     Button(
-        modifier = Modifier
+        modifier =
+        Modifier
             .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()),
-        onClick = mainNavHostController::popBackStack
+        onClick = mainNavHostController::popBackStack,
     ) {
         Text(text = "back")
     }
