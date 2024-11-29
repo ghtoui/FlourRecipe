95f9c60a97d690bd7861fcf176050520efa242e1
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-11-29 15:55:23 +0900

    レシピのモデル作成

---
+++ app/src/main/java/com/ghtoui/flourRecipe/model/recipe/BaseIngredient.kt
@@ -0,0 +1,19 @@
+package com.ghtoui.flourRecipe.model.recipe
+
+/**
+ * 基礎となる材料の情報
+ *
+ * 基礎の量毎のカロリーを計算できるようにする
+ * eg: 100cal / 100g -> [calorie]: 100, [baseQuantity]: 100
+ *
+ * @property id ID
+ * @property name 名前
+ * @property calorie カロリー
+ * @property baseQuantity 基礎の量
+ */
+data class BaseIngredient(
+    val id: Int,
+    val name: String,
+    val calorie: Int,
+    val baseQuantity: Int,
+)
---
+++ app/src/main/java/com/ghtoui/flourRecipe/model/recipe/RecipeCategory.kt
@@ -0,0 +1,21 @@
+package com.ghtoui.flourRecipe.model.recipe
+
+/**
+ * レシピのカテゴリ
+ */
+enum class RecipeCategory {
+    /**
+     * パン
+     */
+    Bread,
+
+    /**
+     * 麺
+     */
+    Noodle,
+
+    /**
+     * お菓子
+     */
+    Sweets,
+}
