31268afe576f0adaad75091f3c47d12aec0d2cd2
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-12-20 22:17:09 +0900

    カメラを開くのをモーダルっぽく

--- app/src/main/java/com/ghtoui/flourRecipe/ui/destination/camera/CameraScreenNavigate.kt
+++ app/src/main/java/com/ghtoui/flourRecipe/ui/destination/camera/CameraScreenNavigate.kt
@@ -1,5 +1,9 @@
 package com.ghtoui.flourRecipe.ui.destination.camera
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -12,7 +16,32 @@ import kotlinx.serialization.Serializable
 data object CameraScreenRoute
 
 fun NavGraphBuilder.cameraScreen() {
-    composable<CameraScreenRoute> {
+    composable<CameraScreenRoute>(
+        enterTransition = {
+            slideInVertically(
+                animationSpec = tween(
+                    500,
+                    easing = LinearEasing,
+                ),
+                // デフォルトは上から下なので，下から上に出るようにしたい
+                initialOffsetY = { height ->
+                    height
+                },
+            )
+        },
+        exitTransition = {
+            slideOutVertically(
+                animationSpec = tween(
+                    500,
+                    easing = LinearEasing,
+                ),
+                // デフォルトは上に閉じるので，下に閉じるようにしたい
+                targetOffsetY = { height ->
+                    +height
+                },
+            )
+        },
+    ) {
         CameraScreen()
     }
 }
